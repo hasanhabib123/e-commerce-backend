c4cdd76b16e1a1316376806da96fd620c7203eec
hasanhabib123 <freelancerhasan401@gmail.com>
2023-01-25 13:48:51 +0600

    update

--- app.js
+++ app.js
@@ -8,13 +8,13 @@ const cors = require('cors');
 // Setting up config file
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(cors());
-const corsOptions ={
-    origin:'http://localhost:3000', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
-app.use(cors(corsOptions));
+app.use(cors({ origin: "", credentials: true}));
+// const corsOptions ={
+//     origin:'http://localhost:3000', 
+//     credentials:true,            //access-control-allow-credentials:true
+//     optionSuccessStatus:200
+// }
+// app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 
